513c43e9d5a10a97f67779f21372c6e9f5d3af1d
eggs-benny <benedict.smith87@gmail.com>
2022-11-16 11:47:27 +0000

    fixed initial name test

--- js-jest/test/gilded_rose.test.js
+++ js-jest/test/gilded_rose.test.js
@@ -2,8 +2,8 @@ const {Shop, Item} = require("../src/gilded_rose");
 
 describe("Gilded Rose", function() {
   it("should foo", function() {
-    const gildedRose = new Shop([new Item("foo", 0, 0)]);
+    const gildedRose = new Shop([new Item("+5 Dexterity Vest", 0, 0)]);
     const items = gildedRose.updateQuality();
-    expect(items[0].name).toBe("fixme");
+    expect(items[0].name).toBe("+5 Dexterity Vest");
   });
 });
