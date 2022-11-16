6e53a6715e4806f6b9ba58200429885a58094132
eggs-benny <benedict.smith87@gmail.com>
2022-11-16 11:52:41 +0000

    test implemented for +5 Dexertiy Vest +ve sellIn quality reduction

--- js-jest/test/gilded_rose.test.js
+++ js-jest/test/gilded_rose.test.js
@@ -1,9 +1,11 @@
 const {Shop, Item} = require("../src/gilded_rose");
 
 describe("Gilded Rose", function() {
-  it("should foo", function() {
-    const gildedRose = new Shop([new Item("+5 Dexterity Vest", 0, 0)]);
+  it("should test +5 Dexterity Vest quality reduction for >0 sellIn", function() {
+    const gildedRose = new Shop([new Item("+5 Dexterity Vest", 10, 20)]);
     const items = gildedRose.updateQuality();
     expect(items[0].name).toBe("+5 Dexterity Vest");
+    expect(items[0].sellIn).toBe(9);
+    expect(items[0].quality).toBe(19);
   });
 });
