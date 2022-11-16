b614472e16c4367afc59d1bc568e7ad3567bd88d
eggs-benny <benedict.smith87@gmail.com>
2022-11-16 11:59:08 +0000

    refactored tests, added -ve sellIn ∆ test for vest

--- js-jest/test/gilded_rose.test.js
+++ js-jest/test/gilded_rose.test.js
@@ -1,11 +1,21 @@
 const {Shop, Item} = require("../src/gilded_rose");
 
-describe("Gilded Rose", function() {
-  it("should test +5 Dexterity Vest quality reduction for >0 sellIn", function() {
-    const gildedRose = new Shop([new Item("+5 Dexterity Vest", 10, 20)]);
-    const items = gildedRose.updateQuality();
-    expect(items[0].name).toBe("+5 Dexterity Vest");
-    expect(items[0].sellIn).toBe(9);
-    expect(items[0].quality).toBe(19);
+describe("Gilded Rose", () => {
+  describe('Item: +5 Dexterity Vest', () => {
+    it("should check ∆ quality when sellIn >0", () => {
+      const gildedRose = new Shop([new Item("+5 Dexterity Vest", 10, 20)]);
+      const items = gildedRose.updateQuality();
+      expect(items[0].name).toBe("+5 Dexterity Vest");
+      expect(items[0].sellIn).toBe(9);
+      expect(items[0].quality).toBe(19);
+    })
+    it("should check ∆ quality when sellIn <0", function() {
+      const gildedRose = new Shop([new Item("+5 Dexterity Vest", -1, 8)]);
+      const items = gildedRose.updateQuality();
+      expect(items[0].name).toBe("+5 Dexterity Vest");
+      expect(items[0].sellIn).toBe(-2);
+      expect(items[0].quality).toBe(6);
+    });
+
   });
 });
