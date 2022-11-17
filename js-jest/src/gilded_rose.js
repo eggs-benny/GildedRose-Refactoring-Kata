d39917c8d02c98ac19cac038cebe67dc6027d026
eggs-benny <benedict.smith87@gmail.com>
2022-11-17 10:21:43 +0000

    readability refactor

--- js-jest/src/gilded_rose.js
+++ js-jest/src/gilded_rose.js
@@ -10,36 +10,79 @@ class Shop {
   constructor(items = []) {
     this.items = items;
   }
+
   updateQuality() {
     for (let i = 0; i < this.items.length; i++) {
-      this.items[i].sellIn --
-      switch (this.items[i].name) {
-        case 'Aged Brie':
-          this.items[i].quality ++
-          if (this.items[i].sellIn < 0) {this.items[i].quality ++}
-          break
-        case 'Conjured Mana Cake':
-          this.items[i].quality -= 2
-          if (this.items[i].sellIn < 0) {this.items[i].quality -= 2}
-          break
-        case 'Backstage passes to a TAFKAL80ETC concert':
-          this.items[i].quality ++
-          if (this.items[i].sellIn < 10) {
-            this.items[i].quality ++ 
-          } if (this.items[i].sellIn < 5) {
-            this.items[i].quality ++
-          } if (this.items[i].sellIn < 0) {this.items[i].quality = 0}
-          break
-        default:
-          this.items[i].quality --
-          if (this.items[i].sellIn < 0) {this.items[i].quality -- }
-          break
-      }
-      if (this.items[i].quality > 50) {this.items[i].quality = 50}
-      if (this.items[i].quality < 0) {this.items[i].quality = 0}
-      if (this.items[i].name == 'Sulfuras, Hand of Ragnaros') {this.items[i].quality = 80; this.items[i].sellIn}
+      this.moveToNextDay(i);
+      this.updateItems(i);
     }
     return this.items;
+  }
+
+  moveToNextDay(i) {
+    this.items[i].sellIn--;
+  }
+
+  updateItems(i) {
+    switch (this.items[i].name) {
+      case 'Aged Brie':
+        this.agedBrieQualityRules(i);
+        break;
+      case 'Backstage passes to a TAFKAL80ETC concert':
+        this.backstagePassQualityRules(i);
+        break;
+      case 'Conjured Mana Cake':
+        this.defaultQualityRules(i);
+        this.defaultQualityRules(i);
+        break;
+      default:
+        this.defaultQualityRules(i);
+        break;
+    }
+    this.setMinMaxQuality(i);
+    this.sulfurasQualityRules(i);
+  }
+
+  agedBrieQualityRules(i) {
+    this.items[i].quality++;
+    if (this.items[i].sellIn < 0) {
+      this.items[i].quality++;
+    }
+  }
+
+  backstagePassQualityRules(i) {
+    this.items[i].quality++;
+    if (this.items[i].sellIn < 10) {
+      this.items[i].quality++;
+    }
+    if (this.items[i].sellIn < 5) {
+      this.items[i].quality++;
+    }
+    if (this.items[i].sellIn < 0) {
+      this.items[i].quality = 0;
+    }
+  }
+
+  defaultQualityRules(i) {
+    this.items[i].quality--;
+    if (this.items[i].sellIn < 0) {
+      this.items[i].quality--;
+    }
+  }
+
+  setMinMaxQuality(i) {
+    if (this.items[i].quality > 50) {
+      this.items[i].quality = 50;
+    }
+    if (this.items[i].quality < 0) {
+      this.items[i].quality = 0;
+    }
+  }
+
+  sulfurasQualityRules(i) {
+    if (this.items[i].name == 'Sulfuras, Hand of Ragnaros') {
+      this.items[i].quality = 80;
+    }
   }
 }
 
